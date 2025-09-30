6551af4cb571d65c9b0fb60dba3e11442f1a5236
Ema Love <e.akintayo@alustudent.com>
2025-09-30 06:59:42 -0700

    #

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,8 @@
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,10 @@
+const Square5 = require('./5-square');
+
+class Square extends Square5 {
+  charPrint (c) {
+    const char = c === undefined ? 'X' : c;
+    for (let i = 0; i < this.height; i++) {
+      console.log(char.repeat(this.width));
+    }
+  }
+}
