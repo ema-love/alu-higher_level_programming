2225918b9e267cf8d3f3fdfc231df94cb8dc449d
Erioluwa <e.akintayo@alustudent.com>
2025-09-30 21:48:53 +0200

    #

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 const Square5 = require('./5-square');
 
-class Square extends Square5 {
+module.exports = class Square extends Square5 {
   charPrint (c) {
-    const char = c === undefined ? 'X' : c;
+    const ch = c === undefined ? 'X' : c;
     for (let i = 0; i < this.height; i++) {
-      console.log(char.repeat(this.width));
+      console.log(ch.repeat(this.width));
     }
   }
-}
-
-module.exports = Square;
+};
