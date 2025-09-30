ecebbb3653eef46fc37972bf738b8b3d636324cb
Erioluwa <e.akintayo@alustudent.com>
2025-09-30 21:47:46 +0200

    #

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
@@ -12,6 +12,4 @@ class Rectangle {
       console.log('X'.repeat(this.width));
     }
   }
-}
-
-module.exports = Rectangle;
+};
